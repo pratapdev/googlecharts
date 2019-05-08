94d8ac9a90b0d49ccc2ea67bb7f4979cf8e5004e
durga pratap s <dsatyamsetti@epsvjw.local>
2019-05-08 12:00:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'googleCharts';
+  title = 'Browser market shares at a specific website, 2014';
+   type = 'PieChart';
+   data = [
+      ['Firefox', 45.0],
+      ['IE', 26.8],
+      ['Chrome', 12.8],
+      ['Safari', 8.5],
+      ['Opera', 6.2],
+      ['Others', 0.7] 
+   ];
+   columnNames = ['Browser', 'Percentage'];
+   options = {
+    'legend':'left',
+    'title':'My Big Pie Chart',
+    'is3D':true,
+    'width':400,
+    'height':300
+  };
+   width = 550;
+   height = 400;
 }
